e9d4976b6ffa949ac0358dabaa5966572d4e5666
herrqian <qianjing1993@gmail.com>
2020-04-22 16:56:14 +0200

    improved code coveralls

--- src/test/scala/de/htwg/se/connect_four/model/FileIOSpec.scala
+++ src/test/scala/de/htwg/se/connect_four/model/FileIOSpec.scala
@@ -12,8 +12,14 @@ class FileIOSpec extends WordSpec with Matchers {
   "A FileIO is used to handle input and output from json-file" when {
     val jsonio = new FileIO()
     val grid = new Grid(6,7)
+    val mgrid = new Grid(10,11)
+    val lgrid = new Grid(16,17)
     val playerarray = Array(true,false)
     "it has a save function" in {
+      jsonio.save(lgrid, playerarray)
+      new File("grid.json").exists() should be(true)
+      jsonio.save(mgrid, playerarray)
+      new File("grid.json").exists() should be(true)
       jsonio.save(grid, playerarray)
       new File("grid.json").exists() should be(true)
     }
@@ -21,6 +27,7 @@ class FileIOSpec extends WordSpec with Matchers {
       val (loadgrid, loadplayers) = jsonio.load
       loadgrid should be(Some(grid))
       loadplayers should be(playerarray)
+      new File("grid.json").delete() should be(true)
     }
   }
 
