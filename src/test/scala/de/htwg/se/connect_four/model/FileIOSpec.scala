f177d3849c6215084c58119b5e782b98c644cf71
herrqian <qianjing1993@gmail.com>
2020-04-22 16:47:03 +0200

    added tests for FileIOJson

---
+++ src/test/scala/de/htwg/se/connect_four/model/FileIOSpec.scala
@@ -0,0 +1,27 @@
+package de.htwg.se.connect_four.model
+
+import java.io.File
+
+import de.htwg.se.connect_four.model.fileIOComponent.fileIoJsonImpl.FileIO
+import de.htwg.se.connect_four.model.gridComponent.gridAdvancedImpl.Grid
+import org.scalatest.{Matchers, WordSpec}
+
+import scala.Some
+
+class FileIOSpec extends WordSpec with Matchers {
+  "A FileIO is used to handle input and output from json-file" when {
+    val jsonio = new FileIO()
+    val grid = new Grid(6,7)
+    val playerarray = Array(true,false)
+    "it has a save function" in {
+      jsonio.save(grid, playerarray)
+      new File("grid.json").exists() should be(true)
+    }
+    "it has a load function" in {
+      val (loadgrid, loadplayers) = jsonio.load
+      loadgrid should be(Some(grid))
+      loadplayers should be(playerarray)
+    }
+  }
+
+}
