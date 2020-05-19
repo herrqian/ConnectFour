9c9f2d7a2b72154ce5adb23afc65118d69d76e7d
herrqian <qianjing1993@gmail.com>
2020-05-19 19:18:10 +0200

    deleted some useless class

--- src/main/scala/de/htwg/se/connect_four/controller/controllerComponent/controllerBaseImpl/Gamestate.scala
+++
@@ -1,8 +0,0 @@
-package de.htwg.se.connect_four.controller.controllerComponent.controllerBaseImpl
-
-case class Gamestate(var mystate:Statelike) {
-
-  def changeState(): Unit = {
-    mystate.handle(this)
-  }
-}
--- src/main/scala/de/htwg/se/connect_four/controller/controllerComponent/controllerBaseImpl/Statelike.scala
+++
@@ -1,8 +0,0 @@
-package de.htwg.se.connect_four.controller.controllerComponent.controllerBaseImpl
-
-import de.htwg.se.connect_four.controller.controllerComponent.GameStatus.GameStatus
-
-trait Statelike {
-  var gameStatus:GameStatus
-  def handle(gamestate: Gamestate): Unit
-}
