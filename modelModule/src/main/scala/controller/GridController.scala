d66bb98749c7be337a1706b19069218c0b84c636
herrqian <qianjing1993@gmail.com>
2020-06-02 18:46:09 +0200

    to adjust with game and updated some function in the class

--- modelModule/src/main/scala/controller/GridController.scala
+++ modelModule/src/main/scala/controller/GridController.scala
@@ -2,9 +2,15 @@ package controller
 
 import main.scala.model.gridComponent.GridInterface
 import main.scala.model.gridComponent.gridAdvancedImpl.Grid
+import main.scala.model.gridComponent.gridBaseImpl.{Cell, Field}
 import play.api.libs.json.{JsNumber, JsObject, Json}
+import util.UndoManager
+
+import scala.util.{Failure, Success, Try}
 
 class GridController(var grid :GridInterface) {
+
+  private val undoManager = new UndoManager
 
   object Grids extends Enumeration {
     type Grids = Value
@@ -22,6 +28,37 @@ class GridController(var grid :GridInterface) {
       case Grids.large =>
         grid = new Grid(16,17)
     }
+  }
+
+
+  def setValueToBottom(column: Int, value: Int): Try[Int] = {
+    this.setValueToGrid(grid.col(column), grid.cells.row - 1, column, value)
+  }
+
+  def checkWinner(row: Int, col: Int, stone: Int): Boolean = {
+    grid.is4Stone(row, col, stone)
+  }
+
+  def setValueToGrid(cells: Field, row: Int, col: Int, stone: Int): Try[Int] = {
+    Try(cells.cell(row).equals(Cell(0))) match {
+      case Success(cond) => {
+        if (cond) {
+          undoManager.doStep(new SetCommand(row, col, stone, this))
+          Success(row)
+        } else {
+          setValueToGrid(cells, row - 1, col, stone)
+        }
+      }
+      case Failure(exception) => Failure(exception)
+    }
+  }
+
+  def undo:Unit = {
+    undoManager.undoStep
+  }
+
+  def redo:Unit = {
+    undoManager.redoStep
   }
 
   def gridToJson: JsObject = Json.obj(
