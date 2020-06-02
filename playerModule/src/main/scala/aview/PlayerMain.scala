3539cfe1f30266f47f53a9adae2eebcc106375ce
herrqian <qianjing1993@gmail.com>
2020-06-02 18:46:35 +0200

    updated main and playerhttpserver

--- playerModule/src/main/scala/aview/PlayerMain.scala
+++ playerModule/src/main/scala/aview/PlayerMain.scala
@@ -1,7 +1,22 @@
 package aview
 
-object PlayerMain {
-  def main(args: Array[String]): Unit = {
+import controller.PlayerController
 
-  }
+object PlayerMain {
+    val playercontroller = new PlayerController()
+    val httpserver = new PlayerHttpServer(playercontroller)
+    @volatile var shutdown = false
+
+    def main(args: Array[String]): Unit = {
+      println("http://localhost:22222" + " is open")
+      while (!shutdown) {
+        Thread.sleep(1000)
+        ;
+      }
+
+      httpserver.unbind
+      println("http://localhost:22222" + " is closed")
+    }
+
+    def shutdownServer(): Unit = shutdown = true
 }
